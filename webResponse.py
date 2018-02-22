485630534435333faefe4a61c20b5a86dc1cfaeb
michaelk2101 <mickeyk1674@gmail.com>
2018-02-22 13:38:49 +1000

    made web repsonse display ideas form DB

--- webResponse.py
+++ webResponse.py
@@ -1,15 +1,25 @@
 from flask import Flask, request
 import requests
+import psycopg2
+import os
+from urllib import parse
+
+# Connecting to postgres DATABASE
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+cur = conn.cursor()
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def startup():
     msg = """"""
-    with open('ideas.txt', 'r') as f:
-        data = list(f.readlines())
-    for item in data:
-        msg += '{}<br>'.format(item)
+    cur.execute('SELECT * FROM ideas;')
+    rows = cur.fetchall()
+    for row in rows:
+        msg += '{}<br>'.format(row)
     return msg, 200
 
 if __name__ == '__main__':
